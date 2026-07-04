f00db3987f58d67cfa8d9e6b4f9b4fe608de48bf
nemotato <angelyague.a@gmail.com>
2026-07-04 21:02:35 +0200

    stupid transition added

--- script.js
+++ script.js
@@ -7,41 +7,54 @@ function camaleon() {
   "timesnew",
   "retro",
   "metal",
-  "authoritarian",]
+  "authoritarian"]
   fams = fams.concat(fams.concat(fams.concat(["retro"])))
 
   let intervalo = setInterval(() => {
-  document.getElementById("camaleon").style.fontFamily =  fams[i]
-  i++
-  if (i >= fams.length) {
-    clearInterval(intervalo)
-  }
+    document.getElementById("camaleon").style.fontFamily =  fams[i]
+    i++
+    if (i >= fams.length) {
+      clearInterval(intervalo)
+    }
   }, 100)
 }
-
 // MAKES THE COOL >>>> TRANSITION AFTER CLICKING A LINK
 const a_elements = document.getElementsByTagName("a")
 
 for (let i = 0; i < a_elements.length; i++) {
   a_elements[i].addEventListener("click", fade)
+  console.log(a_elements[i].textContent)
 }
 
 function fade(evento) {
   evento.preventDefault()
   
-  ler interval = setInterval(function() {
-    fader(this, '<')
-  } , 100)
-  window.location.href = this.href
+  console.log("peventdefault se ejecuta")
+  let element = this
+  let i = 0
+  let interval = setInterval(function() {
+    fader(element, '>')
+    fader(document.getElementById("camaleon"), '<')
+    i++
+    if (i >= element.textContent.length) {
+      window.location.href = element.href
+      clearInterval(interval)
+    }
+  }, 100)
 }
 
 function fader(element, character) {
   let string = element.textContent
+  let stringcp = ""
+  let newchar = false
   for (let i = 0; i < string.length; i++) {
-    if (string[i] != character) {
-      string[i] = character
-      break
+    if (string[i] != character && newchar == false ){
+      stringcp += character
+      newchar = true
+    }
+    else {
+      stringcp += string[i]
     }
   }
-  element.textContent = string
+  element.textContent = stringcp
 }
